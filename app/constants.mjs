2e019ec0fe952dcf21d4e836ff9bae7ab3a394bb
whatisaghostwriter <sunracarrera@gmail.com>
2023-12-30 17:00:54 +0000

    :bento: files(remix): add assets

--- app/constants.mjs
+++ app/constants.mjs
@@ -8,22 +8,6 @@ export const imageBaseUrl = "/assets/";
 /**
  * @type {import("@webstudio-is/image").ImageLoader}
  */
-export const imageLoader = (props) => {
-  if (process.env.NODE_ENV !== "production") {
-    return imageBaseUrl + props.src;
-  }
-
-  if (props.format === "raw") {
-    return imageBaseUrl + props.src;
-  }
-
-  // https://vercel.com/blog/build-your-own-web-framework#automatic-image-optimization
-  return (
-    "/_vercel/image?url=" +
-    encodeURIComponent(imageBaseUrl + props.src) +
-    "&w=" +
-    props.width +
-    "&q=" +
-    props.quality
-  );
+export const imageLoader = ({ src }) => {
+  return imageBaseUrl + src;
 };
